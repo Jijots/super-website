810451cba908fef56d1edd8f16659b18cb110669
Jijots <josstuazon@gmail.com>
2026-07-12 18:47:13 +0800

    Add fade/slide page transition animation on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import PageTransition from "./components/PageTransition";
 import Home from "./pages/Home";
 import Company from "./pages/Company";
 import Projects from "./pages/Projects";
@@ -8,17 +9,21 @@ import ProjectDetail from "./pages/ProjectDetail";
 import Contact from "./pages/Contact";
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <div className="flex min-h-screen flex-col bg-cream text-ink">
       <Navbar />
       <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/company" element={<Company />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:slug" element={<ProjectDetail />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <PageTransition key={location.pathname}>
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:slug" element={<ProjectDetail />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </PageTransition>
       </main>
       <Footer />
     </div>
---
+++ src/components/PageTransition.jsx
@@ -0,0 +1,3 @@
+export default function PageTransition({ children }) {
+  return <div className="animate-page-enter">{children}</div>;
+}
